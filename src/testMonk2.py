0ad12b75737e9120e1e494a724ceddd96c539acc
ItaKartor <58552414+itakartor@users.noreply.github.com>
2023-12-09 13:52:43 +0100

    fix

--- src/testMonk2.py
+++ src/testMonk2.py
@@ -8,7 +8,7 @@ winner = kfCV.validate(FineGS = True)
 print(winner.to_string())
 
 # Hold-out Test 2
-model=dnn(**winner.get_dictionary)
+model=dnn(**winner.get_dictionary())
 TS_x_monk2,TS_y_monk2= readMC.get_test_Monk_2()
 model.fit(TR_x_monk2,TR_y_monk2,TR_x_monk2,TR_y_monk2)
 out = model.forward_propagation(TS_x_monk2)
