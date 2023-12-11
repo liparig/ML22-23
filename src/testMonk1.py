f7896e305f0d6e3fb8f4645ccef2de5d5fde0459
ItaKartor <58552414+itakartor@users.noreply.github.com>
2023-12-11 20:19:06 +0100

    identify mains  programs

--- src/testMonk1.py
+++ src/testMonk1.py
@@ -2,19 +2,20 @@ from didacticNeuralNetwork import DidacticNeuralNetwork as dnn
 from kfoldCV import KfoldCV 
 import readMonk_and_Cup as readMC
 
-TR_x_monk1,TR_y_monk1 = readMC.get_train_Monk_1()
-kfCV = KfoldCV(TR_x_monk1, TR_y_monk1, 2)
-winner = kfCV.validate(FineGS = True)
-print(winner.to_string())
+if __name__ == '__main__':
+    TR_x_monk1,TR_y_monk1 = readMC.get_train_Monk_1()
+    kfCV = KfoldCV(TR_x_monk1, TR_y_monk1, 2)
+    winner = kfCV.validate(FineGS = True)
+    print(winner.to_string())
 
-# Hold-out Test 1
-model=dnn(**winner.get_dictionary())
-TS_x_monk1,TS_y_monk1= readMC.get_test_Monk_1()
-model.fit(TR_x_monk1,TR_y_monk1,TR_x_monk1,TR_y_monk1)
-out = model.forward_propagation(TS_x_monk1)
+    # Hold-out Test 1
+    model=dnn(**winner.get_dictionary())
+    TS_x_monk1,TS_y_monk1= readMC.get_test_Monk_1()
+    model.fit(TR_x_monk1,TR_y_monk1,TR_x_monk1,TR_y_monk1)
+    out = model.forward_propagation(TS_x_monk1)
 
-error={}
-error['mean_absolute_error'] = model.metrics.mean_absolute_error(TS_y_monk1, out)
-error['root_mean_squared_error'] = model.metrics.root_mean_squared_error(TS_y_monk1, out)
-error['mean_euclidean_error'] = model.metrics.mean_euclidean_error(TS_y_monk1, out)
-print(error)
+    error={}
+    error['mean_absolute_error'] = model.metrics.mean_absolute_error(TS_y_monk1, out)
+    error['root_mean_squared_error'] = model.metrics.root_mean_squared_error(TS_y_monk1, out)
+    error['mean_euclidean_error'] = model.metrics.mean_euclidean_error(TS_y_monk1, out)
+    print(error)
--- src/testMonk2.py
+++ src/testMonk2.py
@@ -2,18 +2,19 @@ from didacticNeuralNetwork import DidacticNeuralNetwork as dnn
 from kfoldCV import KfoldCV
 import readMonk_and_Cup as readMC
 
-TR_x_monk2,TR_y_monk2 = readMC.get_train_Monk_2()
-kfCV = KfoldCV(TR_x_monk2, TR_y_monk2, 5)
-winner = kfCV.validate(FineGS = True)
-print(winner.to_string())
+if __name__ == '__main__':
+    TR_x_monk2,TR_y_monk2 = readMC.get_train_Monk_2()
+    kfCV = KfoldCV(TR_x_monk2, TR_y_monk2, 5)
+    winner = kfCV.validate(FineGS = True)
+    print(winner.to_string())
 
-# Hold-out Test 2
-model=dnn(**winner.get_dictionary())
-TS_x_monk2,TS_y_monk2= readMC.get_test_Monk_2()
-model.fit(TR_x_monk2,TR_y_monk2,TR_x_monk2,TR_y_monk2)
-out = model.forward_propagation(TS_x_monk2)
-error={}
-error['mean_absolute_error'] = model.metrics.mean_absolute_error(TS_y_monk2, out)
-error['root_mean_squared_error'] = model.metrics.root_mean_squared_error(TS_y_monk2, out)
-error['mean_euclidean_error'] = model.metrics.mean_euclidean_error(TS_y_monk2, out)
-print(error)
+    # Hold-out Test 2
+    model=dnn(**winner.get_dictionary())
+    TS_x_monk2,TS_y_monk2= readMC.get_test_Monk_2()
+    model.fit(TR_x_monk2,TR_y_monk2,TR_x_monk2,TR_y_monk2)
+    out = model.forward_propagation(TS_x_monk2)
+    error={}
+    error['mean_absolute_error'] = model.metrics.mean_absolute_error(TS_y_monk2, out)
+    error['root_mean_squared_error'] = model.metrics.root_mean_squared_error(TS_y_monk2, out)
+    error['mean_euclidean_error'] = model.metrics.mean_euclidean_error(TS_y_monk2, out)
+    print(error)
