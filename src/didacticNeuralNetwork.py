Fix update bias e creato test backprop

src/didacticNeuralNetwork.py
@@ -184,9 +184,10 @@ class DidacticNeuralNetwork:
         p_eta:float = (self.eta / pattern)
         for l in range(len(self.l_dim) - 1, 0, -1):
             name_layer:str = str(l)
-            print(f"Name {name_layer}",self.out[f"out{l-1}"].shape)
+           # print(f"Name {name_layer}",self.out[f"out{l-1}"].shape)
             deltaW = -p_eta * (delta[l-1].T@self.out[f"out{l-1}"])
-            deltaB = -p_eta * (np.sum(delta[l-1]))
+            deltaB = -p_eta * (np.sum(delta[l-1].T,axis=1,keepdims=True))
+            
             #if regularization is set subtract the penalty term
             if self.regular:
                 deltaW -= self.regular.derivative(self, self.lambdar, self.wb[f'W{name_layer}'])
@@ -200,12 +201,15 @@ class DidacticNeuralNetwork:
             # print(type(deltaW))
             #print(f"B of {name_layer}",self.wb[f'b{name_layer}'])
             # print(self.wb[f'b{name_layer}'])
-            print(f"B {name_layer}",self.wb[f'b{name_layer}'])
-            print("Delta B",deltaB, deltaB.shape)
+           # print(f"B {name_layer}",self.wb[f'b{name_layer}'])
+           # print("Delta B",deltaB, deltaB.shape)
             self.wb[f'W{name_layer}']= self.wb[f'W{name_layer}'] + deltaW 
+           # print("PRIMA B"+name_layer, self.wb[f'b{name_layer}'],"deltab",deltaB.shape)
+            
             self.wb[f'b{name_layer}'] = self.wb[f'b{name_layer}'] + deltaB
-            print("B dopo upadate {name_layer}",self.wb[f'b{name_layer}'])
-            input('premi')
+           # print("Dopo B"+name_layer, self.wb[f'b{name_layer}'],  "deltab:",deltaB.shape )
+            #print("B dopo upadate {name_layer}",self.wb[f'b{name_layer}'])
+            #input('premi')
 
             # print(f"W:{self.wb[f'W{name_layer}']}, b:{self.wb[f'b{name_layer}']}")
             # input('premi')